3bebe0c549c2e861f28f270b80b2e26bb2f6c537
Nguyen Hoang <hmnguyen1067@gmail.com>
2025-09-05 16:47:09 +0700

    Add script for generating initial EDA report

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 SHELL = /bin/bash
 
-getdata:
+data:
 	mkdir -p ./data
 	curl -L -o ./data/bank-customer-churn.zip\
   		https://www.kaggle.com/api/v1/datasets/download/radheshyamkollipara/bank-customer-churn
 	unzip ./data/bank-customer-churn.zip -d ./data
 	rm ./data/bank-customer-churn.zip
+
+report: data
+	python ./scripts/initial_report.py
---
+++ scripts/initial_report.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+
+data_path = "data/Customer-Churn-Records.csv"
+save_path = "data/initial_report.html"
+
+records = pd.read_csv(data_path)
+records.drop(columns=["RowNumber", "CustomerId", "Surname"], inplace=True)  
+
+profile = ProfileReport(records, title="Initial Profiling Report", explorative=True)
+
+profile.to_file(save_path)
